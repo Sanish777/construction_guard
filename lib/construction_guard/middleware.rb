5b628c5d69e0e629120d70d5c59f5211410b8987
Sanish Shakya <sanishshakya@bajratechnolgoies.com>
2023-07-27 00:20:26 +0545

    fix: test 2

--- lib/construction_guard/middleware.rb
+++ lib/construction_guard/middleware.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module ConstructionGuard::Middleware
+    puts "========================= Method called=================="
   def initialize(app)
     @app = app
   end
