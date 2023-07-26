00ac38de6f8c34fd1ca4ea705ead175088f7fb0a
Sanish Shakya <sanishshakya@bajratechnolgoies.com>
2023-07-26 23:50:37 +0545

    fix: update middleware

--- lib/construction_guard/middleware.rb
+++ lib/construction_guard/middleware.rb
@@ -1,34 +1,40 @@
 # frozen_string_literal: true
 
-module ConstructionGuard
-  class Middleware
-    def initialize(app)
-      @app = app
-    end
+module ConstructionGuard::Middleware
+  def initialize(app)
+    @app = app
+  end
 
-    def call(env)
-      if under_construction?
-        render_under_construction_page
-      else
-        @app.call(env)
-      end
-    end
+  def call(env)
+    return [200, { 'Content-Type' => 'text/html' }, [under_construction_response]] if under_construction?
 
-    private
+    @app.call(env)
+  end
 
-    def under_construction?
-      # Implement your logic here to check if the application is under construction
-      # For simplicity, you can use an environment variable or a configuration setting
-      # to toggle the under construction state.
-      # Example:
-      ENV['SITE_ARMOR_UNDER_CONSTRUCTION'] == 'true'
-    end
+  private
 
-    def render_under_construction_page
-      # Return a static under construction page.
-      # You can create an HTML file or use an ERB template to render the page.
-      # For simplicity, we'll return a simple response here.
-      [200, { 'Content-Type' => 'text/html' }, ['<html><body>Under Construction!</body></html>']]
-    end
+  def under_construction?
+    # Implement your logic here to check if the application is under construction
+    # For simplicity, you can use an environment variable or a configuration setting
+    # to toggle the under construction state.
+    # Example:
+    true
+  end
+
+  def under_construction_response
+    # The HTML content for the "Under Construction" page.
+    # You can customize this page as you like.
+    <<~HTML
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <title>Under Construction</title>
+        </head>
+        <body>
+          <h1>Under Construction</h1>
+          <p>This website is currently under construction. Please check back later.</p>
+        </body>
+      </html>
+    HTML
   end
 end
