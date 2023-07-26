be64d9f2d1aa0af78ea7536efa1fd8c1ab9d5ec7
Sanish Shakya <sanishshakya@bajratechnolgoies.com>
2023-07-26 23:15:32 +0545

    Initial Commit

---
+++ lib/construction_guard.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "construction_guard/version"
+
+module ConstructionGuard
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/construction_guard_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ConstructionGuard do
+  it "has a version number" do
+    expect(ConstructionGuard::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
