2b71695a802279661a49fbe42096f0bf77feaf9e
Sanish Shakya <sanishshakya@bajratechnolgoies.com>
2023-07-28 21:43:45 +0545

    feat: added self class

--- lib/construction_guard.rb
+++ lib/construction_guard.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "construction_guard/version"
+require "construction_guard/middleware"
 
 module ConstructionGuard
   class Error < StandardError; end
--- lib/construction_guard/middleware.rb
+++ lib/construction_guard/middleware.rb
@@ -1,45 +1,40 @@
 # frozen_string_literal: true
 
 module ConstructionGuard::Middleware
-    puts "========================= Method called=================="
-  def initialize(app)
-    @app = app
-  end
+  class << self
+    def initialize(app)
+      @app = app
+    end
 
-  def call(env)
-    return [200, { 'Content-Type' => 'text/html' }, [under_construction_response]] if under_construction?
+    def call(env)
+      return [200, { 'Content-Type' => 'text/html' }, [under_construction_response]] if under_construction?
 
-    @app.call(env)
-  end
+      @app.call(env)
+    end
 
-  def connection
-    "-------------Connection Established-------------"
-  end
+    def under_construction?
+      # Implement your logic here to check if the application is under construction
+      # For simplicity, you can use an environment variable or a configuration setting
+      # to toggle the under construction state.
+      # Example:
+      true
+    end
 
-  private
-
-  def under_construction?
-    # Implement your logic here to check if the application is under construction
-    # For simplicity, you can use an environment variable or a configuration setting
-    # to toggle the under construction state.
-    # Example:
-    true
-  end
-
-  def under_construction_response
-    # The HTML content for the "Under Construction" page.
-    # You can customize this page as you like.
-    <<~HTML
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <title>Under Construction</title>
-        </head>
-        <body>
-          <h1>Under Construction</h1>
-          <p>This website is currently under construction. Please check back later.</p>
-        </body>
-      </html>
-    HTML
+    def under_construction_response
+      # The HTML content for the "Under Construction" page.
+      # You can customize this page as you like.
+      <<~HTML
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>Under Construction</title>
+          </head>
+          <body>
+            <h1>Under Construction</h1>
+            <p>This website is currently under construction. Please check back later.</p>
+          </body>
+        </html>
+      HTML
+    end
   end
 end
