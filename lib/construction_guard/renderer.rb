e1543bd3db380b8418c7700ebf5f3fd2b913892e
Rakesh Bhatt <bhattrakesh073@gmail.com>
2023-12-12 22:18:01 +0545

    feat: add functionality to set the all the local variables set from the code - add method to show the flash message

--- lib/construction_guard/renderer.rb
+++ lib/construction_guard/renderer.rb
@@ -9,21 +9,33 @@ module ConstructionGuard
         template_path = File.join(File.dirname(__FILE__), 'views', 'default_template.html.erb')
         puts "Template '#{template_name}' not found. Rendering default template."
       end
+
       locals = { title: 'Default Title', greeting: 'Hello' }.merge(options)
+      flash_messages = locals.delete(:flash) || {} # Extract flash messages
 
       # Read the ERB template file
       erb_template = File.read(template_path)
       binding_with_locals = binding
-      binding_with_locals.local_variable_set(:message, options[:message])
+
+      # Set locals in the binding
+      locals.each { |key, value| binding_with_locals.local_variable_set(key, value) }
+
       # Render the ERB template
       renderer = ERB.new(erb_template)
-      renderer.result(binding_with_locals)
+      rendered_template = renderer.result(binding_with_locals)
+
+      # Inject flash messages into the rendered template
+      inject_flash_messages(rendered_template, flash_messages)
     end
 
     def self.find_template(template_name)
       gem_root = Gem.loaded_specs["construction_guard"].full_gem_path
       template_path = File.join(gem_root, "lib", "construction_guard", 'views', "#{template_name}.html.erb")
       File.exist?(template_path) ? template_path : nil
+    end
+
+    def self.inject_flash_messages(template, flash_messages)
+      template + "\n" + flash_messages.map { |key, value| "<div class='flash #{key}'>#{value}</div>" }.join("\n")
     end
   end
 end
